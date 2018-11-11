c4ac987c5ff9b36494c91743525cee56ee6a64dd
Tom Bialick <tombialick@Toms-iMac.local>
2018-11-11 17:36:39 -0500

    Changed sensors and interval

--- Capstone/ViewController.swift
+++ Capstone/ViewController.swift
@@ -17,6 +17,12 @@ class ViewController: UIViewController {
     var x = 0.0
     var y = 0.0
     var z = 0.0
+    var gyroX = 0.0
+    var gyroY = 0.0
+    var gyroZ = 0.0
+    var thetaX = 0.0
+    var thetaY = 0.0
+    var thetaZ = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,9 +32,64 @@ class ViewController: UIViewController {
         motionManager.startMagnetometerUpdates()
         motionManager.startDeviceMotionUpdates()
         
-        timer = Timer.scheduledTimer(timeInterval: 0.25, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
     }
     @objc func update() {
+        if let g_data = motionManager.gyroData, let m_data = motionManager.magnetometerData {
+            
+            gyroX = g_data.rotationRate.x
+            gyroY = g_data.rotationRate.y
+            gyroZ = g_data.rotationRate.z
+            thetaX = m_data.magneticField.x
+            thetaY = m_data.magneticField.y
+            thetaZ = m_data.magneticField.z
+            
+            let parameters = ["gx": String(gyroX), "gy": String(gyroY), "gz": String(gyroZ), "tx": String(thetaX), "ty": String(thetaY), "tz": String(thetaZ)]
+            //create the url with URL
+            let url = URL(string: "http://5halfcap.ngrok.io/phone")!
+            
+            //create the session object
+            let session = URLSession.shared
+            
+            //now create the URLRequest object using the url object
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST" //set http method as POST
+            
+            do {
+                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            
+            //create dataTask using the session object to send data to the server
+            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+                
+                guard error == nil else {
+                    return
+                }
+                
+                guard let data = data else {
+                    return
+                }
+                
+                do {
+                    //create json object from data
+                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
+                        print(json)
+                        // handle json...
+                    }
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            })
+            task.resume()
+            
+        }
+
+        /*
         if let data = motionManager.accelerometerData {
             x = data.acceleration.x
             y = data.acceleration.y
@@ -76,7 +137,7 @@ class ViewController: UIViewController {
                 }
             })
             task.resume()
-        }/*
+        }*//*
         if let gyroData = motionManager.gyroData {
             print(gyroData)
         }
