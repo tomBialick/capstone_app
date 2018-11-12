4c9d052c8ee7af2e6b0a0de5a5eca7bfba320dc6
Tom Bialick <tombialick@Toms-iMac.local>
2018-11-11 19:23:03 -0500

    added altitude

--- Capstone/ViewController.swift
+++ Capstone/ViewController.swift
@@ -11,7 +11,9 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
-    @IBOutlet var textField: UITextField!
+    @IBOutlet weak var wavePicker: UISegmentedControl!
+    @IBOutlet weak var phonePicker: UISegmentedControl!
+    
     let motionManager = CMMotionManager()
     var timer: Timer!
     var x = 0.0
@@ -23,6 +25,9 @@ class ViewController: UIViewController {
     var thetaX = 0.0
     var thetaY = 0.0
     var thetaZ = 0.0
+    var altitude = 0.0
+    
+    lazy var altimeter = CMAltimeter()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,7 +49,42 @@ class ViewController: UIViewController {
             thetaY = m_data.magneticField.y
             thetaZ = m_data.magneticField.z
             
-            let parameters = ["gx": String(gyroX), "gy": String(gyroY), "gz": String(gyroZ), "tx": String(thetaX), "ty": String(thetaY), "tz": String(thetaZ)]
+            altimeter.startRelativeAltitudeUpdates(to: OperationQueue.main, withHandler: { (altitudeData:CMAltitudeData?, error:Error?) in
+            
+                self.altitude = Double(altitudeData!.relativeAltitude.floatValue)
+            })
+            var waveChoice = "sine"
+            switch (wavePicker.selectedSegmentIndex) {
+            case 0:
+                waveChoice = "sine"
+                break
+            case 1:
+                waveChoice = "square"
+                break
+            case 2:
+                waveChoice = "sawtooth"
+                break;
+            case 3:
+                waveChoice = "triangle"
+                break
+            default:
+                waveChoice = "sine"
+            }
+            
+            var phoneChoice = "0"
+            switch(phonePicker.selectedSegmentIndex) {
+            case 0:
+                phoneChoice = "0"
+                break
+            case 1:
+                phoneChoice = "1"
+                break
+            default:
+                phoneChoice = "0"
+                break
+            }
+            
+            let parameters = ["phone": phoneChoice, "wave": waveChoice, "gx": String(gyroX), "gy": String(gyroY), "gz": String(gyroZ), "tx": String(thetaX), "ty": String(thetaY), "tz": String(thetaZ), "altitude": "\(altitude)"]
             //create the url with URL
             let url = URL(string: "http://5halfcap.ngrok.io/phone")!
             
@@ -88,116 +128,8 @@ class ViewController: UIViewController {
             task.resume()
             
         }
-
-        /*
-        if let data = motionManager.accelerometerData {
-            x = data.acceleration.x
-            y = data.acceleration.y
-            z = data.acceleration.z
-            
-            let parameters = ["x": String(x), "y": String(y), "z": String(z)]
-            //create the url with URL
-            let url = URL(string: "http://5halfcap.ngrok.io/phone")! //change the url
-            
-            //create the session object
-            let session = URLSession.shared
-            
-            //now create the URLRequest object using the url object
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST" //set http method as POST
-            
-            do {
-                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-            } catch let error {
-                print(error.localizedDescription)
-            }
-            
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-            
-            //create dataTask using the session object to send data to the server
-            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-                
-                guard error == nil else {
-                    return
-                }
-                
-                guard let data = data else {
-                    return
-                }
-                
-                do {
-                    //create json object from data
-                    if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                        print(json)
-                        // handle json...
-                    }
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            })
-            task.resume()
-        }*//*
-        if let gyroData = motionManager.gyroData {
-            print(gyroData)
-        }
-        if let magnetometerData = motionManager.magnetometerData {
-            print(magnetometerData)
-        }
-        if let deviceMotion = motionManager.deviceMotion {
-            print(deviceMotion)
-        }*/
     }
 
-
-    @IBAction func sendButtonTapped(_ sender: Any) {
-        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
-        
-        let parameters = ["text": textField.text!]
-        
-        //create the url with URL
-        let url = URL(string: "http://5halfcap.ngrok.io/")! //change the url
-        
-        //create the session object
-        let session = URLSession.shared
-        
-        //now create the URLRequest object using the url object
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST" //set http method as POST
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-        } catch let error {
-            print(error.localizedDescription)
-        }
-        
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-        //create dataTask using the session object to send data to the server
-        let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-            
-            guard error == nil else {
-                return
-            }
-            
-            guard let data = data else {
-                return
-            }
-            
-            do {
-                //create json object from data
-                if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                    print(json)
-                    // handle json...
-                }
-            } catch let error {
-                print(error.localizedDescription)
-            }
-        })
-        task.resume()
-    }
-    
     
 }
 
